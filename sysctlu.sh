1a9fcd738a1432fca85cc67ded42819de2fcc6a7
odt0 <odt0@yandex.ru>
2022-07-26 16:51:41 +0300

    r

--- sysctlu.sh
+++ sysctlu.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+readarray -t parametres < <(cat file.txt | awk {'print $1'})
+readarray -t value < <(cat file.txt | awk {'print $3, $4, $5'})
+readarray -t hosts < <(cat hosts)
+
+for i in ${!array[@]};
+  do
+    echo 
+        echo "${parametres[$i]}"
+	echo "${value[$i]}"
+	echo "${hosts[$i]}"
+	echo "------"
+
+  done
+
+
 if [ -z $1 ];
 then
 	VAL_NET_CORE_WMEM_MAX=10485760
@@ -25,7 +40,7 @@ PARAM=$2
 # $3 - новое значение
 VAL=$3
 
-# echo "$(grep -c $PARAM $FILE)"
+echo "$(grep -c $PARAM $FILE)"
 
 if [[ $(grep -c ^$PARAM $FILE) -gt 0 ]] && [[ $(grep -Po '^${PARAM}*=*\K.*' $FILE) != $VAL ]];
       then
@@ -38,8 +53,35 @@ if [[ $(grep -c ^$PARAM $FILE) -gt 0 ]] && [[ $(grep -Po '^${PARAM}*=*\K.*' $FIL
 fi
 }
 
-parametres=("net.core.wmem_max" "net.core.rmem_max" "net.ipv4.tcp_wmem" "net.ipv4.tcp_rmem" "net.ipv4.route.flush")
-value=("${VAL_NET_CORE_WMEM_MAX}" "${VAL_NET_CORE_RMEM_MAX}" "${VAL_NET_IPV4_TCP_WMEM}" "${VAL_NET_IPV4_TCP_RMEM}" "${VAL_NET_IPV4_ROUTE_FLUSH}")
+
+replace_insert_ssh() {
+# Replace in file.conf
+# $1 - host ssh
+HOST=$1
+# $2 - путь к файлу
+FILE=$2
+# $3 - праметр
+PARAM=$3
+# $4 - новое значение
+VAL=$4
+
+echo "$(ssh ${HOST} -T 'cat ${FILE}' | grep -c ${PARAM})"
+
+if [[ $(ssh ${HOST} -T "cat ${FILE}" | grep -c ^${PARAM}) -gt 0 ]] && [[ $(sh ${HOST} -T "cat ${FILE}" | grep -Po '^${PARAM}*=*\K.*') != ${VAL} ]];
+      then
+          echo "replace $(ssh ${HOST} -T 'cat ${FILE}' | grep ^${PARAM}) to ${PARAM}=${VAL} in file ${FILE} on host ${HOST}"
+         ssh ${HOST} -T "sed -i 's/\(^${PARAM} *= *\).*/\1${VAL}/' ${FILE}"
+  elif [ $(ssh ${HOST} -T "cat ${FILE}" | grep -c ^${PARAM}) = 0 ];
+      then
+              echo "insert ${PARAM} in file ${FILE} on host: ${HOST}"    
+          echo "${PARAM}=${VAL}" | ssh ${HOST} -T "cat >> ${FILE}"
+fi
+}
+
+
+
+#parametres=("net.core.wmem_max" "net.core.rmem_max" "net.ipv4.tcp_wmem" "net.ipv4.tcp_rmem" "net.ipv4.route.flush")
+#value=("${VAL_NET_CORE_WMEM_MAX}" "${VAL_NET_CORE_RMEM_MAX}" "${VAL_NET_IPV4_TCP_WMEM}" "${VAL_NET_IPV4_TCP_RMEM}" "${VAL_NET_IPV4_ROUTE_FLUSH}")
 
 for i in ${!parametres[@]};
   do
@@ -51,5 +93,17 @@ for i in ${!parametres[@]};
 echo
 echo
 echo
+
+for ((a=0 ; a < ${#hosts[@]}; a++)) do
+echo "${hosts[$a]}"
+   for i in ${!parametres[@]};
+    do
+     echo 
+        echo "${parametres[$i]}=${value[$i]} host: ${hosts[$a]}"
+        replace_insert_ssh "${hosts[$a]}" "/home/student/sysctl.conf" "${parametres[$i]}" "${value[$i]}"
+    done
+
+done
+
 
 sysctl -p
